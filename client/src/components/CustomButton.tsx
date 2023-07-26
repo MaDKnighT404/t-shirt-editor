aa73984d13d3e908ca56406e00a24755b3b38ceb
George Koloidi <Battleaxe@bk.ru>
2023-07-26 19:09:51 +0200

    feat: add Home component

---
+++ client/src/components/CustomButton.tsx
@@ -0,0 +1,36 @@
+import { useSnapshot } from 'valtio';
+import state from '../store';
+
+const CustomButton = ({
+  type,
+  title,
+  customStyles,
+  handleClick,
+}: {
+  type: string;
+  title: string;
+  customStyles: string;
+  handleClick: () => void;
+}) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type: string) => {
+    if (type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#fff',
+      };
+    }
+  };
+
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
