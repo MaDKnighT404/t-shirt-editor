2bd719f921f94ed925ebaf4f7843d534133639ae
George Koloidi <Battleaxe@bk.ru>
2023-07-27 13:20:05 +0200

    fix: shadow

--- client/src/canvas/Backdrop.tsx
+++ client/src/canvas/Backdrop.tsx
@@ -10,7 +10,7 @@ const Backdrop = () => {
       position={[0, 0, -0.14]}
       temporal
       frames={60}
-      alphaTest={0.5}
+      alphaTest={0.85}
       scale={10}
       rotation={[Math.PI / 2, 0, 0]}
     >
@@ -18,7 +18,7 @@ const Backdrop = () => {
         amount={4}
         radius={9}
         intensity={0.55}
-        ambient={0.25}
+        ambient={0.35}
         position={[5, 5, -10]}
       />
       <RandomizedLight
@@ -33,3 +33,39 @@ const Backdrop = () => {
 };
 
 export default Backdrop;
+
+// import { useRef } from 'react'
+// import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+
+// const Backdrop = () => {
+//   const shadows = useRef(null);
+
+//   return (
+//     <AccumulativeShadows
+//       ref={shadows}
+//       temporal
+//       frames={60}
+//       alphaTest={0.85}
+//       scale={10}
+//       rotation={[Math.PI / 2, 0, 0]}
+//       position={[0, 0, -0.14]}
+//     >
+//       <RandomizedLight 
+//         amount={4}
+//         radius={9}
+//         intensity={0.55}
+//         ambient={0.25}
+//         position={[5, 5, -10]}
+//       />
+//       <RandomizedLight 
+//         amount={4}
+//         radius={5}
+//         intensity={0.25}
+//         ambient={0.55}
+//         position={[-5, 5, -9]}
+//       />
+//     </AccumulativeShadows>
+//   )
+// }
+
+// export default Backdrop
