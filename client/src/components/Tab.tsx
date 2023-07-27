a8e4fd8970f6bf762cc3fc33fdb5472a5e9e2fee
George Koloidi <Battleaxe@bk.ru>
2023-07-27 13:52:58 +0200

    feat: add ColorPicker

--- client/src/components/Tab.tsx
+++ client/src/components/Tab.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { useSnapshot } from 'valtio';
 
-const Tab = () => {
-  return <div>Tab</div>;
+import state from '../store';
+
+type Tab = {
+  name: string;
+  icon: string;
+};
+
+const Tab = ({
+  tab,
+  isFilterTab,
+  isActiveTab,
+  handleClick,
+}: {
+  tab: Tab;
+  isFilterTab?: boolean;
+  isActiveTab?: boolean;
+  handleClick: () => void;
+}) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColod: snap.color, opacity: 0.5 }
+      : { backgroundColor: 'transpetent', opacity: 1 };
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rouded-4'}`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${isFilterTab} ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'`}
+      />
+    </div>
+  );
 };
 
 export default Tab;
--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 import { download } from '../assets';
@@ -11,6 +11,31 @@ import state from '../store';
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+  const [promt, setPromt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab content depending on the acriveTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />;
+      case 'filepicker':
+        return <FilePicker />;
+      case 'aipicker':
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -23,8 +48,15 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {
+                      setActiveEditorTab(tab.name);
+                    }}
+                  />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -40,12 +72,7 @@ const Customizer = () => {
 
           <motion.div className="filtertabs-container" {...slideAnimation('up')}>
             {FilterTabs.map((tab) => (
-              <Tab
-                key={tab.name}
-                tab={tab}
-                handleClick={() => { }}
-                isFilterTab
-                isActiveTab="" />
+              <Tab key={tab.name} tab={tab} handleClick={() => {}} isFilterTab isActiveTab />
             ))}
           </motion.div>
         </>
