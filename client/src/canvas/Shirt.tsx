3323c71fa5698e1711dcfa5bd3255fddb343f48a
George Koloidi <Battleaxe@bk.ru>
2023-07-27 13:12:56 +0200

    feat: add shadow to shirt

--- client/src/canvas/Shirt.tsx
+++ client/src/canvas/Shirt.tsx
@@ -2,24 +2,49 @@ import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import { GLTF } from 'three-stdlib';
 import state from '../store';
+
+type GLTFResult = GLTF & {
+  nodes: { T_Shirt_male: THREE.Mesh };
+  materials: { lambert1: THREE.MeshStandardMaterial };
+};
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const { nodes, materials } = useGLTF('/shirt_baked.glb') as GLTFResult;
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((_, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {snap.isFullTexture && (
+          <Decal position={[0, 0, 0]} rotation={[0, 0, 0]} scale={1} map={fullTexture} />
+        )}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            depthTest={false}
+            // map-anisotropy={16}
+            // depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   );
 };
